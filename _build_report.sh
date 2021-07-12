deedd4533cfddf1dbb0e6466e2225ae255fff357
nikosbosse <nikosbosse@gmail.com>
2021-07-12 14:21:31 +0200

    update report

---
+++ _build_report.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+Rscript -e "rmarkdown::render('upgrading-report.Rmd', 'bookdown::pdf_book')"
+
+Rscript -e 'pdftools::pdf_combine(input = c("upgrading-report.pdf", "Approval Letter Ethics PhD.pdf", "Ethics aplication PhD.pdf", "Research_Ethics_online_training_certificate.pdf", "Data-Management-Plan.pdf"), output = "upgrading-report-nikos-bosse.pdf")'
