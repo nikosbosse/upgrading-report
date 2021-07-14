8d66ce16c63683d7fdf13be4c933ded5c5aa7107
nikosbosse <nikosbosse@gmail.com>
2021-07-14 19:11:10 +0200

    reorganise files

--- _build_report.sh
+++ _build_report.sh
@@ -2,6 +2,8 @@
 
 Rscript -e "rmarkdown::render('upgrading-report.Rmd', 'bookdown::pdf_book')"
 
-Rscript -e 'pdftools::pdf_combine(input = c("upgrading-report.pdf", "Approval Letter Ethics PhD.pdf", "Ethics aplication PhD.pdf", "Research_Ethics_online_training_certificate.pdf", "Data-Management-Plan.pdf"), output = "upgrading-report-nikos-bosse.pdf")'
+Rscript -e 'pdftools::pdf_combine(input = c("upgrading-report.pdf", "supplementary/Approval Letter Ethics PhD.pdf", "supplementary/Ethics aplication PhD.pdf", "supplementary/Research_Ethics_online_training_certificate.pdf", "supplementary/Data-Management-Plan.pdf"), output = "upgrading-report-nikos-bosse.pdf")'
+
+rm upgrading-report.pdf
 
 Rscript -e "rmarkdown::render('upgrading-report.Rmd', 'word_document')"
